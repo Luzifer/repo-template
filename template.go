2ac6bd136615ad2633bafaeb0520ec9269bb72ec
Knut Ahlers <knut@ahlers.me>
2019-04-15 20:39:24 +0200

    Add has_topic filter for conditional templates
    Signed-off-by: Knut Ahlers <knut@ahlers.me>

--- template.go
+++ template.go
@@ -4,10 +4,12 @@ import (
 	"strings"
 
 	"github.com/flosch/pongo2"
+	"github.com/google/go-github/github"
 	"github.com/gosimple/slug"
 )
 
 func init() {
+	pongo2.RegisterFilter("has_topic", tplRepoHasTopic)
 	pongo2.RegisterFilter("groovy_save", tplGroovyFileSave)
 	pongo2.RegisterFilter("slugify", tplSlugify)
 }
@@ -18,4 +20,24 @@ func tplSlugify(in, param *pongo2.Value) (*pongo2.Value, *pongo2.Error) {
 
 func tplGroovyFileSave(in, param *pongo2.Value) (*pongo2.Value, *pongo2.Error) {
 	return pongo2.AsValue(strings.Replace(in.String(), "-", "_", -1)), nil
+}
+
+func tplRepoHasTopic(in, param *pongo2.Value) (*pongo2.Value, *pongo2.Error) {
+	repo, ok := in.Interface().(*github.Repository)
+	if !ok {
+		return nil, &pongo2.Error{
+			Sender:   "filter:has_topic",
+			ErrorMsg: "Input was no github.Repository",
+		}
+	}
+
+	topic := param.String()
+
+	for _, t := range repo.Topics {
+		if t == topic {
+			return pongo2.AsValue(true), nil
+		}
+	}
+
+	return pongo2.AsValue(false), nil
 }
